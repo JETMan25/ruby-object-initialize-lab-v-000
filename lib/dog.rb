6122ae6bfc3e9431a87125c22ba4f0954c1a4fcf
jontenthoff <jontenthoff@hotmail.com>
2018-09-17 13:50:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,13 @@
 class Dog 
+<<<<<<< HEAD
   def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
+=======
+  def initialize(name, breed)
+    @name = name
+    @breed = "Mutt"
+>>>>>>> 87cfe82543cc1ee3ccde5018e8c142db50044a38
     end
     def name=(name)
       @name = name
@@ -9,6 +15,12 @@ class Dog
   def name
     @name 
   end
+<<<<<<< HEAD
+=======
+  def breed(breed)
+    @breed = breed
+  end
+>>>>>>> 87cfe82543cc1ee3ccde5018e8c142db50044a38
   def breed=(breed)
     @breed = breed
   end
